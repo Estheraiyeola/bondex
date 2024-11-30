9087b96d46d9b57b6fc2f21dd56b3e5297f1ab91
Yisaterese <teresejosephyisa@gmail.com>
2024-11-30 17:08:45 +0100

    fixed build fail

--- bondex/app/community/esusu/page.tsx
+++ bondex/app/community/esusu/page.tsx
@@ -1,11 +1,12 @@
-import Navbar from "@/components/navbar/Navbar";
+import React from 'react';
 import EsusuCommunity from "../../../components/community/modal/esusu/Esusu";
+import Navbar from "../../../components/navbar/Navbar";
 
-export default function page(){
-    return(
+export default function Page() {
+    return (
         <div>
-         <Navbar/>
-           <EsusuCommunity/>
+            <Navbar />
+            <EsusuCommunity />
         </div>
     );
 }
